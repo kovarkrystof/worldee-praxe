90bf30d38a5b76aadcca54356028d8554b48b311
Kryštof Kovář <krystofkovar@Krystofs-Laptop.local>
2025-05-05 15:01:46 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+var firstNuber = 123;
+var secondNumber = 321;
+console.log(firstNuber + secondNumber);
+
+
+let age: number = 18
+let firstName: string = "Krystof"
+let adultness: boolean = false
+
+if(age >= 18){
+    adultness = true
+}
+
+if(adultness === true){
+    console.log(`${firstName} is adult.`)
+}   else {
+    console.log(`${firstName} is kid.`)
+}
