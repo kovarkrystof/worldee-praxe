bf9f6d4fa3c98c5a77cec1f657bc152f71141c13
Kryštof Kovář <krystofkovar@Krystofs-Laptop.local>
2025-05-07 09:21:41 +0200

    ukol 6 vyreseny

--- runner.ts
+++ runner.ts
@@ -3,8 +3,8 @@
 // console.log(require('./files/ukol_2'));
 // console.log(require('./files/ukol_3'));
 // console.log(require('./files/ukol_4'));
-console.log(require('./files/ukol_5'));
-// console.log(require('./files/ukol_6'));
+// console.log(require('./files/ukol_5'));
+console.log(require('./files/ukol_6'));
 // console.log(require('./files/ukol_7'));
 // console.log(require('./files/ukol_8'));
 // console.log(require('./files/ukol_9'));
